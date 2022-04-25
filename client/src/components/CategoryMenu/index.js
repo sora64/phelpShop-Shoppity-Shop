d0b80843646cef8f3d2702ee98701c65c56654fe
Alan Phelps <phelpsa64@gmail.com>
2022-04-25 13:23:24 -0400

    users can view and filter products by category and can click on a product to see full details

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_CATEGORIES } from "../../utils/queries";
 import { useStoreContext } from "../../utils/GlobalState";
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,7 +6,7 @@ import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif";
 
-function ProductList({ currentCategory }) {
+function ProductList() {
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
@@ -35,7 +35,7 @@ function ProductList({ currentCategory }) {
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
