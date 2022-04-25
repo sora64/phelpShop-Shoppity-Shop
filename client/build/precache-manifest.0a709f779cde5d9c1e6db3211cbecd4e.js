8327bcd8cd89df8a8cd057778313771ef9a1e0ca
Alan Phelps <phelpsa64@gmail.com>
2022-04-25 13:11:42 -0400

    Global State, yeah baby

---
+++ client/build/precache-manifest.0a709f779cde5d9c1e6db3211cbecd4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3299642847730212aafd510ed1b8a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6c5f3794cb7487bf07c",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "c2a8cde5360be2f1cd1b",
+    "url": "/static/js/2.c00bf566.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c00bf566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c5f3794cb7487bf07c",
+    "url": "/static/js/main.238ecf84.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
